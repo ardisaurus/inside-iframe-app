407fdb8fd0880ecf9d9b24c71f87ec9abfcd5edd
Nugroho Ardi Sutrisno <nugroho@prospace.io>
2024-10-03 01:58:53 +0700

    update add set B cookie

--- src/components/CookiesTest.jsx
+++ src/components/CookiesTest.jsx
@@ -1,9 +1,33 @@
+import { useEffect } from "react";
+
+var CookieName = "TestCookie";
+
 export default function CookiesTest() {
-  var CookieName = "TestCookie";
-  document.cookie = "CookieName=TestCookie;samesite=None; secure";
-  if (document.cookie.indexOf(CookieName) == -1) {
-    return <div>no</div>;
-  } else {
-    return <div>yes</div>;
-  }
+  useEffect(() => {
+    document.cookie = "CookieName=TestCookie;samesite=None; secure";
+  }, []);
+
+  const handleButtonClick = () => {
+    // const cookieValue = 'foo'; // Set the cookie value to 'foo'
+    // document.cookie = `myCookie=${cookieValue}; path=/; expires=${new Date(new Date().getTime() + 3600000).toUTCString()}`;
+    document.cookie =
+      "CookieItem=fool;samesite=None; secure; max-age=3153600000";
+  };
+
+  return (
+    <div>
+      A cookie : {document.cookie.indexOf(CookieName) == -1 ? "no" : "yes"}
+      <div>
+        <p>B cookie : {document.cookie.indexOf("fool") == -1 ? "no" : "yes"}</p>
+        <button
+          type="button"
+          onClick={() => handleButtonClick()}
+          className="btn yellow continue btn-yellow-hover"
+          style={{ padding: ".4em", margin: ".2em" }}
+        >
+          set B Cookie
+        </button>
+      </div>
+    </div>
+  );
 }
