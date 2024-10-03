9627ea373f5d71d9b6b201f7eafc001f6cf4c4db
Nugroho Ardi Sutrisno <nugroho@prospace.io>
2024-10-03 08:26:38 +0700

    add c cookie method

--- src/components/CookiesTest.jsx
+++ src/components/CookiesTest.jsx
@@ -13,6 +13,12 @@ export default function CookiesTest() {
     document.cookie =
       "CookieItem=fool;samesite=None; secure; max-age=3153600000";
   };
+  const handleButton2Click = () => {
+    // const cookieValue = 'foo'; // Set the cookie value to 'foo'
+    // document.cookie = `myCookie=${cookieValue}; path=/; expires=${new Date(new Date().getTime() + 3600000).toUTCString()}`;
+    document.cookie =
+      "your=cookie; Domain=phenomenal-lollipop-87c933.netlify.app; Path=/; Expires=Thu, 22 Dec 2027 04:17:44 GMT; HttpOnly; Secure; SameSite=Lax";
+  };
 
   return (
     <div>
@@ -26,6 +32,19 @@ export default function CookiesTest() {
           style={{ padding: ".4em", margin: ".2em" }}
         >
           set B Cookie
+        </button>
+      </div>
+      <div>
+        <p>
+          C cookie : {document.cookie.indexOf("cookie") == -1 ? "no" : "yes"}
+        </p>
+        <button
+          type="button"
+          onClick={() => handleButton2Click()}
+          className="btn yellow continue btn-yellow-hover"
+          style={{ padding: ".4em", margin: ".2em" }}
+        >
+          set C Cookie
         </button>
       </div>
     </div>
