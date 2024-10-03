34f7d6961838b1fcfd9c7d35491ef284801078d2
Nugroho Ardi Sutrisno <nugroho@prospace.io>
2024-10-03 08:29:38 +0700

    update domain in c method

--- src/components/CookiesTest.jsx
+++ src/components/CookiesTest.jsx
@@ -17,7 +17,7 @@ export default function CookiesTest() {
     // const cookieValue = 'foo'; // Set the cookie value to 'foo'
     // document.cookie = `myCookie=${cookieValue}; path=/; expires=${new Date(new Date().getTime() + 3600000).toUTCString()}`;
     document.cookie =
-      "your=cookie; Domain=phenomenal-lollipop-87c933.netlify.app; Path=/; Expires=Thu, 22 Dec 2027 04:17:44 GMT; HttpOnly; Secure; SameSite=Lax";
+      "your=cookie; Domain=https://phenomenal-lollipop-87c933.netlify.app/; Path=/; Expires=Thu, 22 Dec 2027 04:17:44 GMT; HttpOnly; Secure; SameSite=Lax";
   };
 
   return (
